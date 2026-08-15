a3392068b046502507865ab5426201ab48d0ccf4
cheesecakecodez <codewithsuhani@gmail.com>
2026-08-15 23:08:25 +0530

    debugging

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -43,85 +43,87 @@ class _LoginPageState extends State<LoginPage> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(25.0),
-          child: Column(
-            //logo
-            children: [
-              const SizedBox(height: 150.0,),
-              Icon(
-                Icons.lock_open_rounded,
-                size: 70, 
-                color:Theme.of(context).colorScheme.primary),
-                const SizedBox(height:50),
-            //welcome back msg
-              Text("Welcome back, You've been missed!",
-              style: TextStyle( 
-                color: Theme.of(context).colorScheme.primary,
-                fontSize: 16)),
-          
-          
-              const SizedBox(height: 25.0),
-            //email textfield
-          
-                  MyTextField(
-                    controller: emailController,
-                    hintText: "Enter email",
-                    obscureText: false,
-                  ),
-                
-                const SizedBox(height: 10,),
-            //password textfield
-          
-            MyTextField(controller: pwController, 
-            hintText: "Enter password", 
-            obscureText: true),
-
-            const SizedBox(height: 10,),
-
-            //forgot password?
-            Align(alignment: Alignment.centerRight,
-              child: Text("Forgot password?",
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.primary,
-                fontWeight: FontWeight.bold,
-              )
-              )
-              ),
-
-              const SizedBox(height: 25),
-
-            //sign in button
-            MyButton(name:"Login", onTap: (){
-                //pop the menu drawer
-                Navigator.pop(context);
-                //navigate to settings page
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage(),
-                )
-                );
-              },),
-              const SizedBox(height: 50),
-            //not a member?register now
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          child: SingleChildScrollView(
+            child: Column(
+              //logo
               children: [
-                
-                  Text("Not a member?", style: TextStyle(color: Theme.of(context).colorScheme.primary)),
-                SizedBox(width: 5),
-                GestureDetector(
-                  onTap:() {
-                    //pop the menu drawer
-                Navigator.pop(context);
-                //navigate to settings page
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> RegisterPage(),
+                const SizedBox(height: 150.0,),
+                Icon(
+                  Icons.lock_open_rounded,
+                  size: 70, 
+                  color:Theme.of(context).colorScheme.primary),
+                  const SizedBox(height:50),
+              //welcome back msg
+                Text("Welcome back, You've been missed!",
+                style: TextStyle( 
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 16)),
+            
+            
+                const SizedBox(height: 25.0),
+              //email textfield
+            
+                    MyTextField(
+                      controller: emailController,
+                      hintText: "Enter email",
+                      obscureText: false,
+                    ),
+                  
+                  const SizedBox(height: 10,),
+              //password textfield
+            
+              MyTextField(controller: pwController, 
+              hintText: "Enter password", 
+              obscureText: true),
+            
+              const SizedBox(height: 10,),
+            
+              //forgot password?
+              Align(alignment: Alignment.centerRight,
+                child: Text("Forgot password?",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
                 )
-                );
-                  },
-                  child: Text("Register Now",
-                  style: TextStyle(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold)
-                  ),
+                )
                 ),
-              ],
-            )
-          ],),
+            
+                const SizedBox(height: 25),
+            
+              //sign in button
+              MyButton(name:"Login", onTap: (){
+                  //pop the menu drawer
+                  Navigator.pop(context);
+                  //navigate to settings page
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage(),
+                  )
+                  );
+                },),
+                const SizedBox(height: 50),
+              //not a member?register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  
+                    Text("Not a member?", style: TextStyle(color: Theme.of(context).colorScheme.primary)),
+                  SizedBox(width: 5),
+                  GestureDetector(
+                    onTap:() {
+                      //pop the menu drawer
+                  Navigator.pop(context);
+                  //navigate to settings page
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> RegisterPage(),
+                  )
+                  );
+                    },
+                    child: Text("Register Now",
+                    style: TextStyle(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold)
+                    ),
+                  ),
+                ],
+              )
+            ],),
+          ),
         ),
       ),
     );
